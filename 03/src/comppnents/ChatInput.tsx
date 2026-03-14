8de6802f3d39e8f4eacf846ee8cd2d438175e129
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-03-14 18:59:09 +0530

    feta:console chta input

--- 03/src/comppnents/ChatInput.tsx
+++ 03/src/comppnents/ChatInput.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
+"use client"
+
+import { useState } from "react"
 
 const ChatInput = () => {
+
+    const [text, setText] = useState("")
+
+    const handleSend = (e:any)=>{
+        e.preventDefault();
+
+        if(!text.trim()) return;
+        console.log("message", text)
+
+        setText("");
+    }
   return (
-    <div className='border-t p-4'>
+    <form onSubmit={handleSend} className='border-t p-4'>
       <input 
         type="text"
+        value={text}
+        onChange={(e)=>setText(e.target.value)}
         placeholder='Send a message....'
         className='w-full border rounded-lg px-4 py-2  outline-none'
        />
-    </div>
+    </form>
   )
 }
 
