b810c9e46c35a0d32f68de7eb15e0f04ae274904
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-03-14 14:06:21 +0530

    feat: hanlde basic memory

--- 01/index.js
+++ 01/index.js
@@ -1,28 +1,40 @@
 // import ollama  from "ollama";
 import OpenAI from "openai";
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
 const client = new OpenAI({
-    baseURL: "https://openrouter.ai/api/v1",
-    apiKey: process.env.OPENROUTER_API_KEY
-})
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey: process.env.OPENROUTER_API_KEY,
+});
 
-async function main(){
+const messages = [
+  {
+    role: "system",
+    content: "You are a helpful AI tutor.",
+  },
+];
+
+async function chat(userInput) {
+    messages.push({
+        role: "user",
+        content: userInput
+    })
+
     const response = await client.chat.completions.create({
         model: "meta-llama/llama-3-8b-instruct",
-        messages:[
-            {
-                role: "system",
-                content:"You are a funny teacher who explains things in a simple way."
-            },
-            {
-                role: "user",
-                content: "Explain Artificial Intelligence"
-            }
-        ]
+        messages
     })
-    const aiResponse = response.choices[0].message.content
-    console.log(aiResponse)
+      const aiResponse = response.choices[0].message.content;
+      messages.push({
+        role: "assistant",
+        content: aiResponse
+      })
+      console.log(aiResponse);
 }
-main()
+
+async function main() {
+  await chat("Who is Elon Musk?");
+  await chat("Where was he born?");
+}
+main();
