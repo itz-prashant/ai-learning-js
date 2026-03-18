60e4433d981e43d792abbd262869f716ac59375b
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-03-18 14:09:30 +0530

    feat : craete login form

--- 04/src/app/(auth)/login/page.tsx
+++ 04/src/app/(auth)/login/page.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (res?.ok) {
+      console.log("Login success");
+    } else {
+      console.log("Login failed");
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex flex-col gap-4 p-10">
+      <h1 className="text-2xl font-bold">Login</h1>
 
-export default page
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="border p-2"
+      />
+
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="border p-2"
+      />
+
+      <button
+        onClick={handleLogin}
+        className="bg-black text-white p-2 border cursor-pointer"
+      >
+        Login
+      </button>
+    </div>
+  );
+}
